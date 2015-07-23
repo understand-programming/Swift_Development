a7998e7877d8f1605dc87d6dcc1f73bda47788e9
Abid Khan <understandprogramming@gmail.com>
2015-07-23 15:44:36 +0200

    Introduction to Swift Programming Language

---
+++ Variables/Variables.playground/Contents.swift
@@ -0,0 +1,52 @@
+/* Strings */
+
+var myFirstName = "Abid" // String
+var myLastName = "Khan" // String
+
+var myFullName = myFirstName + " " + myLastName // Add two strings
+
+
+/* Integers */
+
+var myAge = 25
+var myPostCode = 12345
+
+
+/* Float */
+
+var myWeight = 65.545
+
+/* Bool */
+
+var isActive = true
+
+
+// declare variables explicitly
+
+var myInteger   : Int
+var myString    : String
+var myFloat     : Float
+
+myInteger = 10
+myString  = "My String"
+myFloat   = 100.101
+
+
+var myIntAndValue : Int = 10
+var myStringAndValue : String = "Hello"
+
+
+// compiler will give you error if it variable will be reassign with different data type.
+// uncomment the following line and you will see the compiler error
+//myInteger = "minteger"
+
+
+
+
+
+
+
+
+
+
+
