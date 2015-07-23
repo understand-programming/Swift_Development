165ac98347e66681fc29db108c65d77b0021554d
Abid Khan <understandprogramming@gmail.com>
2015-07-23 15:49:02 +0200

    Constants in Swift

---
+++ Constants/Constant.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// Declare constant with word 'let'
+
+let myName = "Khan"
+
+let myPostCode = 12345
+
+let myTableName = "PERSON"
+
+
+
+// As we know that constant's value cannot be changed, once it is assigned.
+// uncoment the follwoing line and it will give you error.
+//myName = "Carl"
+
+
+// Type Annotation variable : String (variable is typeof String)
+
+let str : String
+str = "Khan"
+
+
+let pie = 3.14159
+
+
+
