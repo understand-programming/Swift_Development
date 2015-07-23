5581a448a5cf00797c104e72eb9628012bdb24a1
Abid Khan <understandprogramming@gmail.com>
2015-07-23 15:56:50 +0200

    Print line in Swift

---
+++ Println/println.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+
+var myName = "Khan"
+
+var country = "Sweden"
+
+var myPostCode = 12345
+
+
+// its very easy to print some information on screen as compare to other languages.
+
+println("My Name is \(myName), i live in \(country) and my postcode is \(myPostCode)")
+
+// this type of syntax is called string interpolation, to insert some you data in exisitng string.
+// you can do string interpolation with small bracket and backslash.
+
+
+
+var patrolperliter = 2
+var quantity = 10
+
+println("You bought \(quantity) liters of patrol and your total amount is \(patrolperliter * quantity) dollors")
+
+
+println(myName)
